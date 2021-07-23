656b70d94df4d093f458200af8ba340b52d8d5b0
kumarjay <kumarjay2107@gmail.com>
2021-07-23 16:41:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.decomposition import PCA
+
+
