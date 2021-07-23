2ae6a57789052e350db90fdd01385b5f63930fbb
kumarjay <kumarjay2107@gmail.com>
2021-07-23 21:29:54 +0530

    cb nd d  jascnmnc

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
 
+train= pd.read_csv('data/train.csv')
 
+x_train, x_test, y_train, _test= train_test_split(x)
